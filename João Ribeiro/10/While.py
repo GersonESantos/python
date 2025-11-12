0dc29d9bcb77e9973964eeae84c28acb0936983b
GersonESS <gebhsantos@gmail.com>
2025-11-12 13:21:00 -0300

    Create While.py

---
+++ João Ribeiro/10/While.py
@@ -0,0 +1,13 @@
+valor = 1
+while valor <= 10:
+    print(valor)
+    valor += 1   
+print("Acabou!")
+# Esse código imprime os números de 1 a 10 e depois exibe a mensagem "Acabou!".
+valor = 1
+while valor <= 10:
+    print(valor)
+    if valor == 5:
+        break
+    valor += 1   
+print("Acabou!")
